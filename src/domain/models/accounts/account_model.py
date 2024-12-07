a09cb10e99478c0f06a905c75a608febffc465d4
Vinicius Reis <v_reis@outlook.com.br>
2024-12-07 13:33:30 -0300

    fix: type mapper

--- src/domain/models/accounts/account_model.py
+++ src/domain/models/accounts/account_model.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+from decimal import Decimal
 
 from sqlalchemy import DECIMAL, UUID, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -13,10 +14,10 @@ class AccountModel(Base):
     account_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    balance: Mapped[float] = mapped_column(
-        DECIMAL(precision=10, scale=2), default=0.00
+    balance: Mapped[Decimal] = mapped_column(
+        DECIMAL(precision=10, scale=2), default=Decimal('0.00')
     )
-    branch_id: Mapped[int] = mapped_column(
+    branch_id: Mapped[str] = mapped_column(
         String(4), nullable=False, default='0001'
     )
     created_at: Mapped[datetime] = mapped_column(default=datetime.now())
